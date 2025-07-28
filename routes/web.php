e806825af3d17709931116d861a68c7a3ef1e608
Alonsou29 <alonsourdaneta.29@hotmail.com>
2025-07-28 13:56:12 -0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('Occupations');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
